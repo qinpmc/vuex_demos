a7ac012e505ee155fd1b7b44c43a7f5e6450daf0
qinpmc <850823973@qq.com>
2021-07-28 11:01:03 +0800

    add getter note

--- vuex_module/src/App.vue
+++ vuex_module/src/App.vue
@@ -1,95 +1,42 @@
 <template>
   <div id="app">
-     <h3>{{name}}</h3>
-     <h3>{{aliasName}}</h3>
-     <h3>{{nameDetail}}</h3>
-     <h3>{{countFullName}}</h3>
-     <button @click="modifyNameAction">修改名字</button>
-
+    <h3>{{gt18}}</h3>
+    <h3>{{gt18Length}}</h3>
+    <br>别名：
+    <h3>{{aliasName}}</h3>
+    <br>
+    <span>根据指定名称获取该人年龄</span>
+    <input type="text" placeholder="请输入姓名" v-model="name">
+    <h3>{{getAgeFromName}}岁</h3>
   </div>
 </template>
 
 <script>
-import {mapState, mapActions,mapMutations,mapGetters} from 'vuex';
+import { mapGetters } from "vuex";
 
-/*
-      export default {
-        data() {
-            return {
-                name: 'Lucy'
-            }
-        },
-
-        methods: {
-            modifyNameAction() {
-                this.name = "bighone"
-            }
-        }
-    }
-*/
-    export default {
-
-        data:function(){
-          return {
-            localName:"local Big"
-          }
-        },
-        //映射的计算属性的名称与 state 的子节点名称相同时，我们也可以给 mapState 传一个字符串数组
-        /*
-        computed: {
-          // 映射 this.name 为 store.state.name
-            ...mapState(['name'])
-        },*/
-
-        /*
-        computed: mapState({
-          // 箭头函数可使代码更简练
-          name: state => state.name,
-
-          // 传字符串参数 'name' 等同于 `state => state.name`
-          aliasName: 'name',
-
-          // 为了能够使用 `this` 获取局部状态，必须使用常规函数
-          countFullName (state) {
-            return state.name +" " +this.localName
-          }
-        }),
-        */
-        computed: {
-            ...mapState({
-                // 
-                name: state => state.moduleA.text,
-
-                //  
-                aliasName: state => state.moduleB.text,
-
-                // 为了能够使用 `this` 获取局部状态，必须使用常规函数
-                countFullName (state) {
-                  return state.name +" " +this.localName
-                }
-            }),
-            ...mapGetters({
-                //nameDetail: 'detail'
-                nameDetail: 'moduleA/detail'
-            }),
-        },
-        methods: {
-            ...mapActions(['modifyName']),
-
-            //...mapActions(['callAction']),
-
-            ...mapActions({
-                callAction:"moduleA/callAction"
-            }),
-            ...mapMutations(['setText']),
-            modifyNameAction() {
-                //this.modifyName('bighone');
-                //this.setText();
-                 this.callAction();
-            }
-        },
-    }
-
+export default {
+  data: function() {
+    return {
+      name: "wangw"
+    };
+  },
+  computed: {
+    // gt18() {
+    //   return this.$store.getters.gt18;
+    // },
+    // gt18Length() {
+    //   return this.$store.getters.gt18Length;
+    // },
+    getAgeFromName() {
+      return this.$store.getters.getSomeoneAge(this.name);
+    },
+    ...mapGetters(["gt18", "gt18Length"]),
+    ...mapGetters({
+      aliasName: "gt18Length"
+    })
+  },
+  methods: {}
+};
 </script>
 
 
--- vuex_module/src/store/index.js
+++ vuex_module/src/store/index.js
@@ -1,25 +1,60 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import moduleA from "./moduleA"
-import moduleB from "./moduleB"
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    moduleA, moduleB,
+
   },
   state: {
-    name: 'Lucy',
+    count: 13,
+    anotherCount: 17,
+    array: [
+      {
+        name: "lis",
+        age: 19
+      },
+      {
+        name: "zhangs",
+        age: 2
+      },
+      {
+        name: "wangw",
+        age: 22
+      },
+      {
+        name: "zhaol",
+        age: 17
+      }
+    ]
+  },
+  getters: {
+    gt18: state => {
+      let gt18Array = state.array.filter((item) => {
+        return item.age > 18;
+      })
+      return gt18Array;
+    },
+    gt18Length: (state, getters) => {
+      return getters.gt18.length;
+    },
+    // 根据传入的name查询数据, 数据存在, 返回数据中的name
+    getSomeoneAge(state) {
+      return (name) => {
+        let oneData = state.array.find((ele) => {
+          return ele.name == name;
+        });
+        let age = oneData ? oneData.age : "查无此人"
+        return age;
+      }
+    }
   },
   mutations: {
-    setName(state, newName) {
-      state.name = newName;
-    }
+
   },
   actions: {
-    modifyName({ commit }, newName) {
-      commit('setName', newName);
-    }
+
   },
 })
